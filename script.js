03dc835ccef8dae7da91fddefec7946f1a5e66d8
Juan Martin Severino <juanmartinseverino@outlook.com>
2022-05-05 09:50:20 -0300

    incorporar arrays

--- script.js
+++ script.js
@@ -1,210 +1,26 @@
-let nombre;
-let nombredia;
-let cantidadcarne;
-let cantidadchedar;
-let cantidadcebolla;
-let cantidadtomate;
-let cantidadlechuga;
-let cantidadbacon;
-let cantidadhuevo;
-let cantidadmayonesa;
-let cantidadketchup;
-let cantidadmostaza;
-let cantidadbarbacoa;
-let carne;
-let chedar;
-let cebolla;
-let tomate;
-let lechuga;
-let bacon;
-let huevo;
-let mayonesa;
-let ketchup;
-let mostaza;
-let barbacoa;
-let precio;
-let preciodescuento;
-let preciofinal;
-
-const preciopan = 100;
-const preciocarne = 200;
-const preciochedar = 45;
-const preciocebolla = 30;
-const preciotomate = 65;
-const preciolechuga = 20;
-const preciobacon = 40;
-const preciohuevo = 20;
-const preciomayonesa = 20;
-const precioketchup = 15;
-const preciomostaza = 15;
-const preciobarbacoa = 20;
-const descuentolunes = 0.3;
-const descuentomiercoles = 0.2;
-const descuentodomingo = 0.1;
-
-do {
-  nombre = ingresarNombre();
-} while (nombre == "");
-
-do {
-  nombredia = ingresarNombredia();
-} while (nombredia == "");
-
-if (
-  nombredia == "lunes" ||
-  nombredia == "Lunes" ||
-  nombredia == "miercoles" ||
-  nombredia == "Miercoles" ||
-  nombredia == "miércoles" ||
-  nombredia == "Miércoles" ||
-  nombredia == "domingo" ||
-  nombredia == "Domingo"
-) {
-  alert(
-    "¡Hola " +
-      nombre +
-      "! \n \n ¡Bienvenido a CoderBurger personalizable! \n \n ¡Armá tu burger soñada! \n \n ¡Hoy es " +
-      nombredia +
-      " de descuentos!"
-  );
-} else {
-  alert(
-    "¡Hola " +
-      nombre +
-      "! \n \n ¡Bienvenido a CoderBurger personalizable! \n \n ¡Armá tu burger soñada! \n \n ¡Hoy es " +
-      nombredia +
-      " de Burger!"
-  );
+class Ingrediente {
+  constructor(nombre, precio, stock) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+  }
 }
 
-do {
-  cantidadcarne = parseInt(prompt("Ingrese la cantidad de carne que desea: "));
-} while (isNaN(cantidadcarne));
-carne = calcularPrecioingrediente(preciocarne, cantidadcarne);
+const pan = new Ingrediente("pan", 100, 50);
+const carne = new Ingrediente("carne", 200, 50);
+const chedar = new Ingrediente("chedar", 45, 50);
+const cebolla = new Ingrediente("cebolla", 100, 50);
+const tomate = new Ingrediente("tomate", 65, 50);
+const lechuga = new Ingrediente("lechuga", 20, 50);
+const bacon = new Ingrediente("bacon", 40, 50);
+const huevo = new Ingrediente("huevo", 20, 50);
+const mayonesa = new Ingrediente("mayonesa", 20, 50);
+const ketchup = new Ingrediente("ketchup", 150, 50);
+const mostaza = new Ingrediente("mostaza", 15, 50);
+const barbacoa = new Ingrediente("barbacoa", 20, 50);
 
-do {
-  cantidadchedar = parseInt(
-    prompt("Ingrese la cantidad de chedar que desea: ")
-  );
-} while (isNaN(cantidadchedar));
-chedar = calcularPrecioingrediente(preciochedar, cantidadchedar);
-
-do {
-  cantidadcebolla = parseInt(
-    prompt("Ingrese la cantidad de cebolla que desea: ")
-  );
-} while (isNaN(cantidadcebolla));
-cebolla = calcularPrecioingrediente(preciocebolla, cantidadcebolla);
-
-do {
-  cantidadtomate = parseInt(
-    prompt("Ingrese la cantidad de tomate que desea: ")
-  );
-} while (isNaN(cantidadtomate));
-tomate = calcularPrecioingrediente(preciotomate, cantidadtomate);
-
-do {
-  cantidadlechuga = parseInt(
-    prompt("Ingrese la cantidad de lechuga que desea: ")
-  );
-} while (isNaN(cantidadlechuga));
-lechuga = calcularPrecioingrediente(preciolechuga, cantidadlechuga);
-
-do {
-  cantidadbacon = parseInt(prompt("Ingrese la cantidad de bacon que desea: "));
-} while (isNaN(cantidadbacon));
-bacon = calcularPrecioingrediente(preciobacon, cantidadbacon);
-
-do {
-  cantidadhuevo = parseInt(prompt("Ingrese la cantidad de huevo que desea: "));
-} while (isNaN(cantidadhuevo));
-huevo = calcularPrecioingrediente(preciohuevo, cantidadhuevo);
-
-do {
-  cantidadmayonesa = parseInt(
-    prompt("Ingrese la cantidad de mayonesa que desea: ")
-  );
-} while (isNaN(cantidadmayonesa));
-mayonesa = calcularPrecioingrediente(preciomayonesa, cantidadmayonesa);
-
-do {
-  cantidadketchup = parseInt(
-    prompt("Ingrese la cantidad de ketchup que desea: ")
-  );
-} while (isNaN(cantidadketchup));
-ketchup = calcularPrecioingrediente(precioketchup, cantidadketchup);
-
-do {
-  cantidadmostaza = parseInt(
-    prompt("Ingrese la cantidad de mostaza que desea: ")
-  );
-} while (isNaN(cantidadmostaza));
-mostaza = calcularPrecioingrediente(preciomostaza, cantidadmostaza);
-
-do {
-  cantidadbarbacoa = parseInt(
-    prompt("Ingrese la cantidad de barbacoa que desea: ")
-  );
-} while (isNaN(cantidadbarbacoa));
-barbacoa = calcularPrecioingrediente(preciobarbacoa, cantidadbarbacoa);
-
-precio = parseInt(
-  calcularPrecioparcial(
-    preciopan,
-    carne,
-    chedar,
-    cebolla,
-    tomate,
-    lechuga,
-    bacon,
-    huevo,
-    mayonesa,
-    ketchup,
-    mostaza,
-    barbacoa
-  )
-);
-console.log("El precio parcial es: $" + precio);
-alert("El precio de tu Burger es de: $" + precio);
-
-if (nombredia == "lunes" || nombredia == "Lunes") {
-  preciodescuento = parseFloat(calcularDescuento(precio, descuentolunes));
-} else if (
-  nombredia == "miercoles" ||
-  nombredia == "Miercoles" ||
-  nombredia == "miércoles" ||
-  nombredia == "Miércoles"
-) {
-  preciodescuento = parseFloat(calcularDescuento(precio, descuentomiercoles));
-} else if (nombredia == "domingo" || nombredia == "Domingo") {
-  preciodescuento = parseFloat(calcularDescuento(precio, descuentodomingo));
-} else {
-  preciodescuento = 0;
-}
-console.log("El descuento es: $" + preciodescuento);
-alert("Por ser " + nombredia + ", el descuento es de: $" + preciodescuento);
-
-preciofinal = parseFloat(calcularPreciofinal(precio, preciodescuento));
-console.log("El precio final es: $" + preciofinal);
-alert("El precio final de tu Burger es: $" + preciofinal);
-
-function ingresarNombre() {
-  let nombre = prompt("Ingrese su nombre");
-  return nombre;
-}
-
-function ingresarNombredia() {
-  let nombredia = prompt("Ingrese nombre del día");
-  return nombredia;
-}
-
-function calcularPrecioingrediente(precioingredientes, cantidadingredientes) {
-  let precioingrediente = precioingredientes * cantidadingredientes;
-  return precioingrediente;
-}
-
-function calcularPrecioparcial(
-  preciopan,
+const ingredientes = [
+  pan,
   carne,
   chedar,
   cebolla,
@@ -215,30 +31,120 @@ function calcularPrecioparcial(
   mayonesa,
   ketchup,
   mostaza,
-  barbacoa
-) {
-  let precio =
-    preciopan +
-    carne +
-    chedar +
-    cebolla +
-    tomate +
-    lechuga +
-    bacon +
-    huevo +
-    mayonesa +
-    ketchup +
-    mostaza +
-    barbacoa;
-  return precio;
+  barbacoa,
+];
+
+const pedido = [];
+
+class Dias {
+  constructor(dia, descuento) {
+    this.dia = dia;
+    this.descuento = descuento;
+  }
 }
 
-function calcularDescuento(precio, descuentodia) {
-  let preciodescuento = precio * descuentodia;
+const lunes = new Dias("lunes", 0.3);
+const miercoles = new Dias("miercoles", 0.2);
+const miercolesb = new Dias("miércoles", 0.2);
+const domingo = new Dias("domingo", 0.1);
+
+const dias = [lunes, miercoles, miercolesb, domingo];
+
+let nombre;
+let dia;
+let precioparcial = 0;
+let preciodescuento;
+let preciofinal;
+
+console.log("Lista de Precios");
+for (const ingrediente of ingredientes) {
+  console.log(ingrediente.nombre + ": $" + ingrediente.precio);
+}
+
+do {
+  nombre = prompt("Ingrese su nombre");
+} while (nombre == "");
+pedido.push(nombre);
+
+console.log(pedido);
+
+do {
+  dia = prompt("Ingrese nombre del día");
+} while (dia == "");
+
+pedido.push(dia.toLowerCase());
+
+console.log(pedido);
+
+const busqueda = dias.find((el) => el.dia == pedido[1]);
+console.log(busqueda);
+console.log(busqueda.descuento);
+
+if (busqueda) {
+  alert(
+    "¡Hola " +
+      pedido[0] +
+      "! \n \n ¡Bienvenido a CoderBurger personalizable! \n \n ¡Armá tu burger soñada! \n \n ¡Hoy es " +
+      pedido[1] +
+      " de descuentos!"
+  );
+} else {
+  alert(
+    "¡Hola " +
+      pedido[0] +
+      "! \n \n ¡Bienvenido a CoderBurger personalizable! \n \n ¡Armá tu burger soñada! \n \n ¡Hoy es " +
+      pedido[1] +
+      " de Burger!"
+  );
+}
+
+for (const ingrediente of ingredientes) {
+  let cantidad;
+  do {
+    cantidad = parseInt(
+      prompt("Ingrese la cantidad de " + ingrediente.nombre + " que desea: ")
+    );
+  } while (isNaN(cantidad));
+
+  if (cantidad != 0) {
+    pedido.push(
+      new Ingrediente(ingrediente.nombre, ingrediente.precio, cantidad)
+    );
+  }
+}
+
+console.log(pedido);
+
+for (const item of pedido) {
+  let precioitem;
+  console.log(item.precio);
+  console.log(item.stock);
+  precioitem = parseFloat(item.precio) * parseFloat(item.stock);
+  console.log(precioitem);
+  if (isNaN(precioitem) != true) {
+    precioparcial += precioitem;
+  }
+}
+
+console.log(precioparcial);
+alert("El precio de tu Burger es de: $" + precioparcial);
+
+preciodescuento = parseFloat(
+  calcularDescuento(precioparcial, busqueda.descuento)
+);
+console.log(preciodescuento);
+alert("Por ser " + pedido[1] + ", el descuento es de: $" + preciodescuento);
+
+preciofinal = parseFloat(calcularPreciofinal(precioparcial, preciodescuento));
+console.log(preciofinal);
+alert("El precio final de tu Burger es: $" + preciofinal);
+
+function calcularDescuento(precioparcial, descuento) {
+  let preciodescuento = precioparcial * descuento;
   return preciodescuento;
 }
 
-function calcularPreciofinal(precio, preciodescuento) {
-  let preciofinal = precio - preciodescuento;
+function calcularPreciofinal(precioparcial, preciodescuento) {
+  let preciofinal = precioparcial - preciodescuento;
   return preciofinal;
 }
