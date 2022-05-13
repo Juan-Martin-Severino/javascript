a585035ab6bf229b98a8d8602e5f4a1a0e302962
Juan Martin Severino <juanmartinseverino@outlook.com>
2022-05-12 21:06:44 -0300

    incorporar eventos

--- script.js
+++ script.js
@@ -51,13 +51,31 @@ const miercolesb = new Dias("miércoles", 0.2);
 const domingo = new Dias("domingo", 0.1);
 
 const dias = [lunes, miercoles, miercolesb, domingo];
+let busqueda;
 
 let nombre;
 let dia;
+
+let cpan;
+let ccarne;
+let cchedar;
+let ccebolla;
+let ctomate;
+let clechuga;
+let cbacon;
+let chuevo;
+let cmayonesa;
+let cketchup;
+let cmostaza;
+let cbarbacoa;
+
+let formularioPedido = document.getElementById("pedido");
+
 let precioparcial = 0;
-let preciodescuento;
+let preciodescuento = 0;
 let preciofinal;
 let divListadePrecios = document.getElementById("listadeprecios");
+let divDetallePedido = document.getElementById("detallepedido");
 
 // INTERACCION CON HTML
 
@@ -74,81 +92,99 @@ for (const ingrediente of ingredientes) {
   console.log(ingrediente.nombre + ": $" + ingrediente.precio);
 }
 
-do {
-  nombre = prompt("Ingrese su nombre");
-} while (nombre == "");
-pedido.push(nombre);
+nombre = document.getElementById("nombre");
+nombre.addEventListener("change", () => {
+  pedido.push(nombre.value);
+  console.log(pedido);
+});
 
-console.log(pedido);
+dia = document.getElementById("dia");
+dia.addEventListener("change", () => {
+  pedido.push(dia.value.toLowerCase());
+  busqueda = dias.find((el) => el.dia == pedido[1]); // UTILIZACION DE MÉTODO DE BUSQUEDA FIND
+  console.log(busqueda);
+  console.log(pedido);
+});
 
-do {
-  dia = prompt("Ingrese nombre del día");
-} while (dia == "");
+cpan = document.getElementById("pan");
+cpan.addEventListener("change", () => {
+  pedido.push(new Ingrediente(pan.nombre, pan.precio, cpan.value));
+  console.log(pedido);
+});
 
-pedido.push(dia.toLowerCase());
+ccarne = document.getElementById("carne");
+ccarne.addEventListener("change", () => {
+  pedido.push(new Ingrediente(carne.nombre, carne.precio, ccarne.value));
+  console.log(pedido);
+});
 
-console.log(pedido);
+cchedar = document.getElementById("chedar");
+cchedar.addEventListener("change", () => {
+  pedido.push(new Ingrediente(chedar.nombre, chedar.precio, cchedar.value));
+  console.log(pedido);
+});
 
-const busqueda = dias.find((el) => el.dia == pedido[1]); // UTILIZACION DE MÉTODO DE BUSQUEDA FIND
-console.log(busqueda);
+ccebolla = document.getElementById("cebolla");
+ccebolla.addEventListener("change", () => {
+  pedido.push(new Ingrediente(cebolla.nombre, cebolla.precio, ccebolla.value));
+  console.log(pedido);
+});
 
-if (busqueda) {
-  alert(
-    "¡Hola " +
-      pedido[0] +
-      "! \n \n ¡Bienvenido a CoderBurger personalizable! \n \n ¡Armá tu burger soñada! \n \n ¡Hoy es " +
-      pedido[1] +
-      " de descuentos!"
+ctomate = document.getElementById("tomate");
+ctomate.addEventListener("change", () => {
+  pedido.push(new Ingrediente(tomate.nombre, tomate.precio, ctomate.value));
+  console.log(pedido);
+});
+
+clechuga = document.getElementById("lechuga");
+clechuga.addEventListener("change", () => {
+  pedido.push(new Ingrediente(lechuga.nombre, lechuga.precio, clechuga.value));
+  console.log(pedido);
+});
+
+cbacon = document.getElementById("bacon");
+cbacon.addEventListener("change", () => {
+  pedido.push(new Ingrediente(bacon.nombre, bacon.precio, cbacon.value));
+  console.log(pedido);
+});
+
+chuevo = document.getElementById("huevo");
+chuevo.addEventListener("change", () => {
+  pedido.push(new Ingrediente(huevo.nombre, huevo.precio, chuevo.value));
+  console.log(pedido);
+});
+
+cmayonesa = document.getElementById("mayonesa");
+cmayonesa.addEventListener("change", () => {
+  pedido.push(
+    new Ingrediente(mayonesa.nombre, mayonesa.precio, cmayonesa.value)
   );
-} else {
-  alert(
-    "¡Hola " +
-      pedido[0] +
-      "! \n \n ¡Bienvenido a CoderBurger personalizable! \n \n ¡Armá tu burger soñada! \n \n ¡Hoy es " +
-      pedido[1] +
-      " de Burger!"
+  console.log(pedido);
+});
+
+cketchup = document.getElementById("ketchup");
+cketchup.addEventListener("change", () => {
+  pedido.push(new Ingrediente(ketchup.nombre, ketchup.precio, cketchup.value));
+  console.log(pedido);
+});
+
+cmostaza = document.getElementById("mostaza");
+cmostaza.addEventListener("change", () => {
+  pedido.push(new Ingrediente(mostaza.nombre, mostaza.precio, cmostaza.value));
+  console.log(pedido);
+});
+
+cbarbacoa = document.getElementById("barbacoa");
+cbarbacoa.addEventListener("change", () => {
+  pedido.push(
+    new Ingrediente(barbacoa.nombre, barbacoa.precio, cbarbacoa.value)
   );
-}
+  console.log(pedido);
+});
 
-for (const ingrediente of ingredientes) {
-  let cantidad;
-  do {
-    cantidad = parseInt(
-      prompt("Ingrese la cantidad de " + ingrediente.nombre + " que desea: ")
-    );
-  } while (isNaN(cantidad));
-
-  if (cantidad != 0) {
-    pedido.push(
-      new Ingrediente(ingrediente.nombre, ingrediente.precio, cantidad)
-    );
-  }
-}
+formularioPedido.addEventListener("submit", mostrarDetalle);
 
 console.log(pedido);
-
-for (const item of pedido) {
-  let precioitem;
-  precioitem = parseFloat(item.precio) * parseFloat(item.stock);
-  if (isNaN(precioitem) != true) {
-    precioparcial += precioitem;
-  }
-}
-
-console.log("El precio de tu Burger es de: $" + precioparcial);
-alert("El precio de tu Burger es de: $" + precioparcial);
-
-preciodescuento = parseFloat(
-  calcularDescuento(precioparcial, busqueda.descuento) //UTILIZACION DE FUNCIONES
-);
-console.log("El precio de descuento es de: $" + preciodescuento);
-alert("Por ser " + pedido[1] + ", el descuento es de: $" + preciodescuento);
-
-preciofinal = parseFloat(calcularPreciofinal(precioparcial, preciodescuento)); //UTILIZACION DE FUNCIONES
-
-console.log("El precio final de tu Burger es: $" + preciofinal);
-alert("El precio final de tu Burger es: $" + preciofinal);
-
 
 // DECLARACION DE FUNCIONES
 
@@ -160,4 +196,41 @@ function calcularDescuento(precioparcial, descuento) {
 function calcularPreciofinal(precioparcial, preciodescuento) {
   let preciofinal = precioparcial - preciodescuento;
   return preciofinal;
+}
+
+function mostrarDetalle(e) {
+  e.preventDefault();
+
+  console.log("hola");
+
+  for (const item of pedido) {
+    let precioitem;
+    precioitem = parseFloat(item.precio) * parseFloat(item.stock);
+    if (isNaN(precioitem) != true) {
+      precioparcial += precioitem;
+    }
+  }
+
+
+  preciofinal = parseFloat(calcularPreciofinal(precioparcial, preciodescuento)); //UTILIZACION DE FUNCIONESFf
+
+  if (busqueda) {
+    preciodescuento = parseFloat(
+      calcularDescuento(precioparcial, busqueda.descuento) //UTILIZACION DE FUNCIONES
+    );
+
+    divDetallePedido.innerHTML += `
+              <h4>Hola ${pedido[0]}! Hoy es ${pedido[1]} de descuentos!</h4>
+              <h4>El precio Parcial de tu burguer es de: $${precioparcial}</h4>
+              <h4>Por ser ${pedido[1]} el descuento es de: $${preciodescuento}</h4>
+              <h4>El precio Final de tu burguer es de: $${preciofinal}</h4>
+        `;
+  } else {
+    divDetallePedido.innerHTML += `
+              <h4>Hola ${pedido[0]}! Hoy es ${pedido[1]} de Burger!</h4>
+              <h4>El precio Parcial de tu burguer es de: $${precioparcial}</h4>
+              <h4>Por ser ${pedido[1]} el descuento es de: $${preciodescuento}</h4>
+              <h4>El precio Final de tu burguer es de: $${preciofinal}</h4>
+`;
+  }
 }
